73076d4d3bf01871c4fcc26214fa86d517cb204a
Ankit Honey <honeyankit@github.com>
2024-08-19 15:43:35 -0700

    Create main-test.js

---
+++ lodash-example/src/main-test.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+
+function getNames(users) {
+  return _.pluck(users, 'user');
+}
+
+// Test function
+function testGetNames() {
+  const users = [
+    { user: 'barney', age: 36, active: true },
+    { user: 'fred', age: 40, active: false }
+  ];
+
+  const expected = ['barney', 'fred'];
+  const result = getNames(users);
+
+  if (_.isEqual(result, expected)) {
+    console.log('Test passed!');
+    process.exit(0); // Success
+  } else {
+    console.log('Test failed!');
+    process.exit(1); // Failure
+  }
+}
+
+testGetNames();
